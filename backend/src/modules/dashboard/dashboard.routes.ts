74ae22ad2cfecd82b00af4f54f89ac07ab10b45f
mukhsinus <kamilovmuhsin5@gmail.com>
2026-03-14 22:25:47 +0500

    backend/modules/dashboard

--- backend/src/modules/dashboard/dashboard.routes.ts
+++ backend/src/modules/dashboard/dashboard.routes.ts
@@ -1,18 +1,28 @@
 // backend/src/modules/dashboard/dashboard.routes.ts
+
 import { FastifyInstance } from "fastify";
 import { authMiddleware } from "../../middleware/auth.middleware";
+
 import {
   getBrandDashboard,
   getCreatorDashboard
-} from "./dashboard.controller";
+} from "./dashboard.controller.js";
 
 export async function dashboardRoutes(app: FastifyInstance) {
+
+  // -----------------------
+  // BRAND DASHBOARD
+  // -----------------------
 
   app.get(
     "/brand",
     { preHandler: authMiddleware },
     getBrandDashboard
   );
+
+  // -----------------------
+  // CREATOR DASHBOARD
+  // -----------------------
 
   app.get(
     "/creator",
